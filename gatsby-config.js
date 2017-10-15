11a06a41484cea6857c33dec1cbf0d01bfffd23f
Calpa Liu <calpaliu@gmail.com>
2017-10-15 13:10:22 +0800

    Update home page sort by date

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/content/_posts/`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { Component } from 'react';
+import Link from 'gatsby-link';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+import Card from '../components/Card'
 
-export default IndexPage
+const HomePage = ({data}) => (
+      <div>
+        <h2>博客文章</h2>
+        {data.allMarkdownRemark.edges.map(({ node }, i) => (
+          <Card
+            title={node.frontmatter.title}
+            date={node.frontmatter.date}
+            key={i}/>
+        ))}
+      </div>
+    );
+
+export default HomePage;
+
+export const pageQuery = graphql`
+  query getAllPosts {
+    allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`;
