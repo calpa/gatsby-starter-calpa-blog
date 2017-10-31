812e65cbceecbf3b1c10dcb80063b56a51618f3f
Calpa Liu <calpaliu@gmail.com>
2017-10-31 10:15:54 +0800

    Install react-instantsearch

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import Link from 'gatsby-link';
+
+import {
+  InstantSearch, Hits, SearchBox,
+  Highlight, Pagination,
+} from 'react-instantsearch/dom';
+
+import { parseDate } from '../api';
+
+const Post = ({ hit }) => (
+  <Link
+    to={`${parseDate(hit.date)}/${hit.slug}`}
+    href={`${parseDate(hit.date)}/${hit.slug}`}
+  >
+    <div style={{ marginTop: '10px' }}>
+      <span className="hit-name">
+        <Highlight attributeName="title" hit={hit} />
+      </span>
+    </div>
+  </Link>
+);
 
 const Search = () => (
-  <h2>Search</h2>
+  <div>
+    <InstantSearch
+      appId="ABS4QV1E9V"
+      apiKey="d9adae0b31ed52fc0f6c4c75aad428bf"
+      indexName="blog"
+    >
+      <SearchBox />
+      <Hits hitComponent={Post} />
+      <Pagination />
+    </InstantSearch>
+    <link rel="stylesheet" href="https://unpkg.com/react-instantsearch-theme-algolia@4.0.0/style.min.css" />
+
+  </div>
 );
 
 export default Search;
