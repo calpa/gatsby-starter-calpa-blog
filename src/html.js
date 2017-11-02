ea3632c41a4276012f07047ac6bb05f558a7f6b5
Calpa Liu <calpaliu@gmail.com>
2017-11-02 20:20:05 +0800

    Remove busuanzi

--- src/html.js
+++ src/html.js
@@ -44,7 +44,6 @@ const HTML = ({
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossOrigin="anonymous" />
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossOrigin="anonymous" />
       <script async src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59311b889457f301" />
-      <script async src="//dn-lbstatics.qbox.me/busuanzi/2.3/busuanzi.pure.mini.js" />
     </body>
   </html>
 );
