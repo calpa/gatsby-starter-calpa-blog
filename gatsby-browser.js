2a8003d9a76cf09a4d8b50757560c90cef005332
Calpa Liu <calpaliu@gmail.com>
2017-11-02 23:56:30 +0800

    Hot fix: addthis refresh after loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,8 @@ ReactGA.initialize(ga_track_id);
 
 exports.onRouteUpdate = (state) => {
   ReactGA.pageview(state.location.pathname);
+  // Fix AddThis refresh problem in SPA
+  if (window.addthis) {
+    window.addthis.layers.refresh();
+  }
 };
