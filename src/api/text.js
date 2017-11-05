012d6733a05be35d3a360c0833b613138117b103
Calpa Liu <calpaliu@gmail.com>
2017-11-05 22:50:45 +0800

    Install highlight.js

--- src/api/text.js
+++ src/api/text.js
@@ -1,4 +1,5 @@
 import marked from 'marked';
+import highlightjs from 'highlight.js';
 import { parseImageTag } from './images';
 
 const getBody = async (mdFile, remark = false) => {
@@ -30,6 +31,15 @@ const getBody = async (mdFile, remark = false) => {
     toc[level] = toc[level] || [];
     toc[level].push(id);
     return `<h${level} id="${id}">${text}</h${level}>`;
+  };
+
+  renderer.code = (code, language = 'javascript') => {
+    // Check whether the given language is valid for highlight.js.
+    const validLang = !!(language && highlightjs.getLanguage(language));
+    // Highlight only if the language is valid.
+    const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+    // Render the highlighted code with `hljs` class.
+    return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
   };
 
   const html = marked(body, { renderer });
--- src/layouts/Head.js
+++ src/layouts/Head.js
@@ -23,6 +23,7 @@ const Head = () => (
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <meta name="google-site-verification" content={config.meta.google_site_verification} />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/solarized-light.min.css" />
   </Helmet>
 );
 
