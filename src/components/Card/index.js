45397d8db9fac0d9eb7a0edf19ccb708e5690132
Calpa Liu <calpaliu@gmail.com>
2017-11-05 09:49:41 +0800

    Reduce Card as stateless component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
 
@@ -14,48 +14,31 @@ const imageStyle = (headerImage, color) => ({
   background: `#${color} url(${parseImgur(headerImage, 'large')}) center/contain no-repeat`,
 });
 
-class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.title = this.props.title;
-    this.date = this.props.date;
-    this.url = this.props.url;
-    this.headerImage = this.props.headerImage;
-    this.headerBackgroundColor = this.props.headerBackgroundColor;
-    this.index = this.props.index;
-    this.theme = 'white';
-  }
-  /*
-  componentDidMount() {
-    const observer = lozad();
-    observer.observe();
-  }
-  */
-  render() {
-    return (
-      <div className="col-sm-12 pb-4">
-        <Link
-          to={parseUrl(this.date, this.url)}
+const Card = ({
+  title, date, url, headerImage, headerBackgroundColor,
+}) => (
+  <div className="col-sm-12 pb-4">
+    <Link
+      to={parseUrl(date, url)}
+      href={parseUrl(date, url)}
+    >
+      <div className="custom-card">
+        <div
+          className="wrapper"
+          style={imageStyle(headerImage, headerBackgroundColor)}
         >
-          <div className="custom-card">
-            <div
-              className={`wrapper ${this.theme === 'white' ? 'white' : 'black'}`}
-              style={imageStyle(this.headerImage, this.headerBackgroundColor)}
-            >
-              <div className="header" />
-              <div className="data">
-                <div className="content">
-                  <div className="stats"><span className="date">{this.date}</span></div>
-                  <h4 className="title">{this.title}</h4>
-                </div>
-              </div>
+          <div className="header" />
+          <div className="data">
+            <div className="content">
+              <div className="stats"><span className="date">{date}</span></div>
+              <h4 className="title">{title}</h4>
             </div>
           </div>
-        </Link>
+        </div>
       </div>
-    );
-  }
-}
+    </Link>
+  </div>
+);
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
@@ -63,7 +46,6 @@ Card.propTypes = {
   url: PropTypes.string.isRequired,
   headerImage: PropTypes.string.isRequired,
   headerBackgroundColor: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
 };
 
 export default Card;
