89ecdd44c2f470dca78dc7888b0a3b67b0b85494
Calpa Liu <calpaliu@gmail.com>
2017-11-09 22:19:27 +0800

    Create personal link icon in sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,12 +3,28 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import config from '../../../data/config';
+
 import Information from './Information';
 
 import './index.scss';
 
+const Icon = ({ href, name, content }) => (
+  <a
+    target="_blank"
+    href={href}
+    rel="external nofollow noopener noreferrer"
+    className="custom-icon"
+  >
+    <span className="fa-stack fa-lg mx-1">
+      <i className="fa fa-circle fa-stack-2x" />
+      <i className={`fa fa-stack-1x fa-inverse ${name}`}>{content}</i>
+    </span>
+  </a>
+);
+
 const Sidebar = ({ post, totalCount, posts }) => (
-  <header className={`my-2 mx-3 intro-header site-heading text-center col-lg-3 col-xs-12 order-lg-1 ${post === true ? 'order-11' : 'order-1'}`} >
+  <header className={`mb-4 mx-3 pb-3 intro-header site-heading text-center col-lg-3 col-xs-12 order-lg-1 ${post === true ? 'order-11' : 'order-1'}`} >
     <div className="about-me">
       <img
         className="avatar my-3"
@@ -18,6 +34,14 @@ const Sidebar = ({ post, totalCount, posts }) => (
       <h4>Calpa</h4>
       <p className="mb-1">夢裡不覺秋已深</p>
       <p className="mb-3">餘情豈是為他人</p>
+      <Icon
+        href={`https://www.zhihu.com/people/${config.zhihu_username}`}
+        content="知"
+      />
+      <Icon
+        href={`https://github.com/${config.github_username}`}
+        name="fa-github"
+      />
       <Information
         totalCount={totalCount}
         posts={posts}
