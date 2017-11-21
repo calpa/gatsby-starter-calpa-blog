1f00afc954e6251874e5363acb112aa9855e0e20
Calpa Liu <calpaliu@gmail.com>
2017-11-21 22:06:33 +0800

    Change navbar style when scroll down

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link, { navigateTo } from 'gatsby-link';
 
 import './index.scss';
@@ -25,36 +25,65 @@ const NavItem = ({ url, name }) => (
   </ul>
 );
 
-const Navbar = ({ location }) => (
-  <nav className="navbar navbar-expand-sm navbar-light sticky-top mb-3 custom-navbar">
-    <Link className="navbar-brand" to="/">Calpa's Blog</Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
+const NavbarClass = [
+  'navbar', 'navbar-expand-md',
+  'sticky-top',
+  'custom-navbar',
+];
 
-    <div className="collapse navbar-collapse flex-row-reverse" id="navbarSupportedContent">
-      {location.pathname !== '/search' &&
-        <form
-          className="form-inline my-2 my-lg-0"
-          onSubmit={e => search(e)}
-        >
-          <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" id="nav-search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.location = this.props.location;
+    this.state = {
+      isTop: true,
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 55;
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop });
       }
+    });
+  }
 
-      <NavItem url="/about/" name="瀏覽簡介" />
-      <NavItem url="/fonts/" name="字體" />
-    </div>
-  </nav>
-);
+  render() {
+    return (
+      <nav className={`${NavbarClass.join(' ')} ${this.state.isTop === true ? 'navbar-light' : 'navbar-dark is-fixed'}`}>
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <span className="brand-logo">Calpa</span> 's Blog
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse flex-row-reverse" id="navbarSupportedContent">
+            {location.pathname !== '/search' &&
+              <form
+                className="form-inline my-2 my-lg-0"
+                onSubmit={e => search(e)}
+              >
+                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" id="nav-search" />
+                <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">
+                  <i className="fa fa-search" />
+                </button>
+              </form>
+            }
+            <NavItem url="/about/" name="瀏覽簡介" />
+            <NavItem url="/fonts/" name="字體" />
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
