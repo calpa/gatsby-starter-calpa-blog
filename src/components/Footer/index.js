941dcb653b040b3e50d3599c88215c3e1af634ac
Calpa Liu <calpaliu@gmail.com>
2017-12-07 21:36:47 +0800

    Display React version in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => (
       <div className="row">
         <div className="col-sm-12 text-center">
           <p className="architecture">
-            Build with <ExternalLink href="https://www.gatsbyjs.org/" title="GatsbyJS" /> and <ExternalLink href="https://reactjs.org/" title="React" />.
+            Build with <ExternalLink href="https://www.gatsbyjs.org/" title="GatsbyJS" /> and <ExternalLink href="https://reactjs.org/" title={`React ${React.version}`} />.
             Hosted on <ExternalLink href="https://www.netlify.com/" title="Netlify" />.<br />
             The code is open source and available at <ExternalLink href="https://github.com/calpa/blog" title="Github" />.
           </p>
