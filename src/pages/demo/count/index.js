b8f9cf4c7bfe51907c729a1ca64e04a5c53dc756
Calpa Liu <calpaliu@gmail.com>
2017-12-28 11:06:09 +0800

    Use react-markmirror to complete the count page

--- src/pages/demo/count/index.js
+++ src/pages/demo/count/index.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Input from '../../../components/Input';
+import Markmirror from 'react-markmirror';
 
-const Count = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col">
-        <h2>字數統計</h2>
-        <Input />
+import { getContent } from '../../../api/text';
+import './index.scss';
+
+class Count extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '',
+      html: '',
+      preview: false,
+      chinese: 0,
+    };
+  }
+
+  async handleChange(code) {
+    const { html } = await getContent(code);
+    const temp = code.match(/[\u4e00-\u9fa5]/g);
+    let chinese = 0;
+
+    if (temp) {
+      chinese = temp.length;
+    }
+
+    this.setState({ code, html, chinese });
+  }
+
+  async togglePreview() {
+    this.setState({ preview: !this.state.preview });
+  }
+
+  render() {
+    return (
+      <div className="container count-page">
+        <div className="row">
+          <div className="col-12">
+            <h2>字數統計</h2>
+            <p>中文字：{this.state.chinese}</p>
+            <button
+              onClick={() => this.togglePreview()}
+              className="btn btn-info"
+            >預覽
+            </button>
+          </div>
+          <div className="col-6">
+            <Markmirror
+              value={this.state.code}
+              onChange={code => this.handleChange(code)}
+            />
+          </div>
+
+          {this.state.preview &&
+            <div className="col-6">
+              <div
+                dangerouslySetInnerHTML={{ __html: this.state.html }}
+              />
+            </div>
+          }
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Count;
