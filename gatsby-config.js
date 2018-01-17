2b261f3b391ce49c5c8890f4459c8c46a4e4b499
Calpa Liu <calpaliu@gmail.com>
2018-01-17 14:28:30 +0800

    Create Rss feed using gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,65 @@ module.exports = {
   pathPrefix: '/',
   siteMetadata: {
     title: 'Calpa&apos;s Blog',
+    description: 'Front End Technical Blog - Calpa',
+    siteUrl: 'https://calpa.me/',
+    author: 'Calpa',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+         {
+          site {
+            siteMetadata {
+              title,
+              description,
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }`,
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulMarkdown } }) =>
+              // GraphQL query the posts from allContentfulMarkdown
+              allContentfulMarkdown.edges.map((edge) => {
+                const url = `${site.siteMetadata.siteUrl}${require('moment')(edge.node.createdDate).locale('zh-hk').format('YYYY/MM/DD')}/${edge.node.url}`;
+                const Remarkable = require('remarkable');
+                const md = new Remarkable({});
+                const description = md.render(edge.node.content);
+
+                return {
+                  title: edge.node.title,
+                  description,
+                  date: require('moment')(edge.node.createdDate).format('MMMM DD, YYYY, h:mm A'),
+                  url,
+                  guid: url,
+                };
+              }),
+            query: `
+              {
+                  allContentfulMarkdown(limit: 10,sort: {fields: [createdDate], order: DESC}) {
+                    edges {
+                      node {
+                        content
+                        title
+                        url
+                        createdDate
+                      }
+                    }
+                  }
+                }
+            `,
+            output: '/feed.xml',
+          },
+        ],
+      },
+    },
     // 'gatsby-plugin-react-next',
     {
       resolve: 'gatsby-plugin-manifest',
