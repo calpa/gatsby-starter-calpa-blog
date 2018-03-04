d6ffb8e7ba879b8016eac445e8c084bf57e3b1d6
Calpa Liu <calpaliu@gmail.com>
2018-03-04 03:23:02 +0000

    Update console message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,6 +9,9 @@ const { ga_track_id } = require('./data/config');
 
 ReactGA.initialize(ga_track_id);
 
+// Inspired by APlayer
+console.log(`${'\n'} %c CALPA %c https://calpa.me ${'\n'}${'\n'}`, 'color: #6cf; background: #030307; padding:5px 0;', 'background: #6cf; padding:5px 0;');
+
 exports.replaceRouterComponent = ({ history }) => {
   const store = createStore();
 
