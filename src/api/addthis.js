27d8881191af44e04ce1f046dea40592f6a31017
Calpa Liu <calpaliu@gmail.com>
2018-08-11 14:59:22 +0800

    feat: remove nouse components

--- src/api/addthis.js
+++
@@ -1,9 +0,0 @@
-import { isBrowser } from './index';
-
-const refreshToolBox = () => (
-  isBrowser ? window.addthis.toolbox('.addthis_toolbox') : false
-);
-
-export default {
-  refreshToolBox,
-};
