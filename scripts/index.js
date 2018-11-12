219bbba9d69d919b19f6461716b856ba6ee1c4a0
Calpa Liu <calpaliu@gmail.com>
2018-11-12 19:28:37 +0800

    feat: remove comments

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,6 @@ const createConfigFile = async () => {
   // TODO: Validate the configJSON
   const configJSON = data.items[0].fields.blog || {};
 
-  // console.log('current config');
-
-  // console.log(configJSON);
   try {
     await fs.writeFile('./data/secret/config.json', JSON.stringify(configJSON));
   } catch (err) {
