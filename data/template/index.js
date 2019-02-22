8a56a1248d07b86528f0c3591127b972e923d666
Calpa Liu <calpaliu@gmail.com>
2019-02-22 21:14:50 +0800

    feat: remove templates and modify slug genereate rule

--- data/template/index.js
+++ data/template/index.js
@@ -1,9 +1,5 @@
-const headers = require('./headers');
-const posts = require('./posts');
 const config = require('./config');
 
 module.exports = {
-  headers,
-  posts,
   config,
 };
--- gatsby/onCreateNode.js
+++ gatsby/onCreateNode.js
@@ -3,7 +3,13 @@ const { createFilePath } = require('gatsby-source-filesystem');
 module.exports = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
   if (node.internal.type === 'MarkdownRemark') {
-    const slug = createFilePath({ node, getNode, basePath: 'pages' });
+    // 如果 markdown 的 frontmatter 裡面有 slug 的話，直接讀就可以了
+    let { slug = '' } = node.frontmatter;
+
+    if (slug === null || slug.trim() === '') {
+      slug = createFilePath({ node, getNode, basePath: 'pages' });
+    }
+
     createNodeField({
       node,
       name: 'slug',
