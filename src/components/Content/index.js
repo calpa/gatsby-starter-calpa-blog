1f35d936b1deba11acdcdc06b6baef09b0ccf357
Calpa Liu <calpaliu@gmail.com>
2019-02-27 13:48:04 +0800

    feat: update friend's link

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,51 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import lozad from 'lozad';
 
-import { isBrowser } from '../../api';
-
-class Content extends Component {
-  constructor(props) {
-    super(props);
-    const { post } = this.props;
-    this.post = post;
-  }
-
-  componentDidMount() {
-    // lazy loads elements with default selector as '.lozad'
-    // Prevent WebPack build fail
-    if (isBrowser()) {
-      // Initialize library
-      const observer = lozad('.lozad', {
-        load(el) {
-          /* eslint-disable no-param-reassign */
-
-          el.src = el.dataset.src;
-          el.onload = () => {
-            el.classList.add('animated');
-            el.classList.add('fadeIn');
-          };
-          /* eslint-enable */
-        },
-      });
-      observer.observe();
-    }
-  }
-
-  render() {
-    const { post } = this.props;
-    return (
-      <div
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: post }}
-        style={{
-          padding: 30,
-          background: 'white',
-        }}
-      />
-    );
-  }
-}
+const Content = ({ post }) => (
+  <div
+    // eslint-disable-next-line react/no-danger
+    dangerouslySetInnerHTML={{ __html: post }}
+    style={{
+      padding: 30,
+      background: 'white',
+    }}
+  />
+);
 
 Content.propTypes = {
   post: PropTypes.string.isRequired,
