ebf437b79e2fe6741ad63135083db537dbf6e4dd
Calpa Liu <calpaliu@gmail.com>
2019-03-28 11:41:56 +0800

    fix: sidebar query

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
-const path = require("path");
+const path = require('path');
 
 const eslintrc = {
-  extends: "airbnb",
-  plugins: ["react", "jsx-a11y", "import"],
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
   settings: {
-    "import/core-modules": [
-      "react",
-      "react-dom",
-      "react-router-dom",
-      "config",
-      "react-helmet",
-      "prop-types"
-    ]
+    'import/core-modules': [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'config',
+      'react-helmet',
+      'prop-types',
+    ],
   },
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "no-shadow": "warn",
-    "no-console": 0,
-    "linebreak-style": 0,
-    "import/extensions": [
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/forbid-prop-types': 0,
+    'no-shadow': 'warn',
+    'no-console': 0,
+    'linebreak-style': 0,
+    'import/extensions': [
       2,
-      "never",
-      { "web.js": "never", json: "never", css: "always" }
+      'never',
+      { 'web.js': 'never', json: 'never', css: 'always' },
     ],
-    "import/no-extraneous-dependencies": [2, { devDependencies: true }],
-    "import/no-unresolved": [2, { ignore: ["antd"] }],
-    "no-underscore-dangle": ["error", { allow: ["_id"] }]
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/no-unresolved': [2, { ignore: ['antd'] }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
   env: {
-    browser: true
-  }
+    browser: true,
+  },
 };
 
 module.exports = eslintrc;
